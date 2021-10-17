e781d7eddf1c76984c93d0319301e7db7f425de5
koumei11 <miketyson40166@gmail.com>
2021-10-17 13:13:30 +0000

    modify

--- conf/run_kubernetes.sh
+++ conf/run_kubernetes.sh
@@ -19,7 +19,6 @@ do
     fi
 done
 
-kubectl port-forward udacitycapstone 8000:80 --address='0.0.0.0' &
-exit 0
+nohup kubectl port-forward udacitycapstone 8000:80 --address='0.0.0.0' &
 
 # kubectl get pods
